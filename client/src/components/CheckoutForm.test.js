5d41ca9c9fa5f22ab61115d7d6ed8453f41e51db
Patrick-G-Oliver <patrickgriffitholiver@gmail.com>
2020-08-16 20:20:12 -0400

    header test passing

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,16 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import App from "../App";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    const { getByText } = render(<App />);
+    const header = getByText(/react plants/i);
+    expect(header).toBeInTheDocument;
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+
+});
