ac0bbfdf2930ae515950ae00693aae35f7af30fa
Patrick-G-Oliver <patrickgriffitholiver@gmail.com>
2020-08-16 20:23:55 -0400

    whoops, switched first test form app header to checkout form header text check

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import App from "../App";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
 test("form header renders", () => {
-    const { getByText } = render(<App />);
-    const header = getByText(/react plants/i);
+    const { getByText } = render(<CheckoutForm />);
+    const header = getByText(/checkout form/i);
     expect(header).toBeInTheDocument;
 });
 
